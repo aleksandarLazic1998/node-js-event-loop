08c2f3a62c94abd10848d11072ee102fe1585044
aleksandar.benefiti <aleksandar@benefiti.rs>
2025-05-14 15:53:06 +0200

    First test with Promise and process.nextTick

--- Readme.md
+++ Readme.md
@@ -3,3 +3,8 @@
 [Event loop representation](/assets/event-loop-representation.png)
 
 ## MicroTask Queue
+
+MicroTask Queue has 2 built in queues inside of it:
+
+1. NextTick Queue
+2. Promise Queue
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 /* MicroTask Queue */
+
+console.log("This is the first item in callstack #1\n");
+
+Promise.resolve().then(() => {
+	console.log("This is the forth item in callstack #4\n");
+	console.log("This is a process in Promise Queue #1\n");
+});
+
+process.nextTick(() => {
+	console.log("This is the third item in callstack #3\n");
+	console.log("This is a process in NextTick Queue #1\n");
+});
+
+console.log("This is the second item in callstack #2\n");
+
+/* Console:
+
+This is the first item in callstack #1
+
+This is the second item in callstack #2
+
+This is the third item in callstack #3
+
+This is a process in NextTick Queue #1
+
+This is the forth item in callstack #5
+
+This is a process in Promise Queue #2
+
+*/
